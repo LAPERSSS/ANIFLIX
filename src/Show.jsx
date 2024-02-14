d94d0490ed3e09e17d04d4965fa71c0554318890
Lanang Arif <theganteng36@gmail.com>
2024-02-14 08:38:52 +0700

    bismillah

--- src/Show.jsx
+++ src/Show.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import App from "./pages/App";
 import TheMovie from "./pages/Anime/[keyword]/TheMovie";
