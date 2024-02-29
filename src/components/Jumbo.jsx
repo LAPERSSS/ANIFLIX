fcfb87b2ee060e6631b2c5ef84cc21e77c06a06c
Lanang Arif <theganteng36@gmail.com>
2024-02-29 19:49:32 +0700

    subhanallah

--- src/components/Jumbo.jsx
+++ src/components/Jumbo.jsx
@@ -6,20 +6,20 @@ function Jumbo(props) {
       <div className="p-4 bg-secondary-subtle">
         <Carousel>
           <Carousel.Item>
-            <div className="carouselImg" style={{ backgroundImage: `url(/ANIFLIX/PUBLIC/${props.movie1})` }}></div>
+            <div className="carouselImg" style={{ backgroundImage: `url(/ANIFLIX/${props.movie1})` }}></div>
             <Carousel.Caption>
               <h3 className="carouselText">ONE PIECE</h3>
             </Carousel.Caption>
           </Carousel.Item>
           <Carousel.Item>
-            <div className="carouselImg" style={{ backgroundImage: `url(/ANIFLIX/PUBLIC/${props.movie2})` }}></div>
+            <div className="carouselImg" style={{ backgroundImage: `url(/ANIFLIX/${props.movie2})` }}></div>
             <Carousel.Caption>
               <h3 className="carouselText">Demon Slayer</h3>
               <p></p>
             </Carousel.Caption>
           </Carousel.Item>
           <Carousel.Item>
-            <div className="carouselImg" style={{ backgroundImage: `url(/ANIFLIX/PUBLIC/${props.movie3})` }}></div>
+            <div className="carouselImg" style={{ backgroundImage: `url(/ANIFLIX/${props.movie3})` }}></div>
             <Carousel.Caption>
               <h3 className="carouselText">My Hero Academia</h3>
               <p></p>
